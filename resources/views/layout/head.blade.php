02b3047136a4e4b4522bac6aa366bc81bd0af138
xamppsul <samsul.marif100@gmail.com>
2025-05-05 11:03:17 +0800

    logo umk

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -2,10 +2,10 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description"
-      content="Multipurpose, super flexible, powerful, clean modern responsive bootstrap 5 admin template">
+    content="Multipurpose, super flexible, powerful, clean modern responsive bootstrap 5 admin template">
 <meta name="keywords"
-      content="admin template, Axelit admin template, dashboard template, flat admin template, responsive admin template, web app">
+    content="admin template, Axelit admin template, dashboard template, flat admin template, responsive admin template, web app">
 <meta name="author" content="la-themes">
-<link rel="icon" href="{{('../assets/images/logo/favicon.png')}}" type="image/x-icon">
-<link rel="shortcut icon" href="{{('../assets/images/logo/favicon.png')}}" type="image/x-icon">
-<title>@yield('title') | Axelit - Premium Admin Template</title>
+<link rel="icon" href="{{ '../assets/images/logo/umk.png' }}" type="image/x-icon">
+<link rel="shortcut icon" href="{{ '../assets/images/logo/umk.png' }}" type="image/x-icon">
+<title>@yield('title') | MD Feb UMK</title>
