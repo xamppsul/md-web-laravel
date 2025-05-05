d4008c7040e1ac9ddf04c94c015ab7f4ffbeb9e9
xamppsul <samsul.marif100@gmail.com>
2025-05-05 16:20:56 +0800

    class depecrated

--- app/Modules/Authentication/usecase/Usecase.php
+++ app/Modules/Authentication/usecase/Usecase.php
@@ -4,8 +4,6 @@ namespace App\Modules\Authentication\usecase;
 
 use App\Modules\Authentication\interfaces\Usecase_intefaces;
 use App\Modules\Authentication\services\Services;
-use App\Src\Constant\Authentication\ConstantAuth;
-use Illuminate\Http\RedirectResponse;
 
 class Usecase extends Services implements Usecase_intefaces
 {
