d35999702c8683e1fa1bebf05a85bfdf46c128cb
xamppsul <samsul.marif100@gmail.com>
2025-05-06 11:47:37 +0800

    500 error template

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,35 @@
-@extends('errors::minimal')
+@section('title', 'Internal Server')
+@include('layout.head')
 
-@section('title', __('Server Error'))
-@section('code', '500')
-@section('message', __('Server Error'))
+@include('layout.css')
+
+<div class="error-container p-0">
+    <div class="container">
+        <div>
+            <div>
+                <img src="{{ asset('../assets/images/background/error-500.png') }}" class="img-fluid" alt=""
+                    width="50%">
+            </div>
+            <div class="mb-3">
+                <div class="row">
+                    <div class="col-lg-8 offset-lg-2">
+                        <p class="text-center text-secondary f-w-500">500 Internal Server Error server error response
+                            code
+                            indicates that the server encountered an unexpected that prevented it from
+                            fulfilling the request</p>
+                    </div>
+                </div>
+            </div>
+            <a role="button" href="{{ route('user.view.login') }}" class="btn btn-lg btn-warning text-white"><i
+                    class="ti ti-home"></i> Back To
+                Home</a>
+        </div>
+    </div>
+</div>
+@section('script')
+    <!--jquery-->
+    <script src="{{ asset('assets/js/jquery-3.6.3.min.js') }}"></script>
+
+    <!-- Bootstrap js-->
+    <script src="{{ asset('assets/vendor/bootstrap/bootstrap.bundle.min.js') }}"></script>
+@endsection
