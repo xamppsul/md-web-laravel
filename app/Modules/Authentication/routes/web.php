a5a84bd0e16a5f25573e6f9a99faddf3301778c8
xamppsul <samsul.marif100@gmail.com>
2025-05-06 11:19:49 +0800

    throttle is limit 30 hit in one minute

--- app/Modules/Authentication/routes/web.php
+++ app/Modules/Authentication/routes/web.php
@@ -3,10 +3,12 @@
 use App\Modules\Authentication\handler\Handler;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [Handler::class, 'viewUserLogin'])->name('user.view.login');
-Route::post('/login', [Handler::class, 'userLogin'])->name('user.do.login');
-Route::post('/forgot-password', [Handler::class, 'userForgotPassword'])->name('user.do.forgot.password');
-Route::prefix('md-admin')->group(function () {
-    Route::get('/', [Handler::class, 'viewAdminLogin'])->name('admin.view.login');
-    Route::post('/login', [Handler::class, 'adminLogin'])->name('admin.do.login');
+Route::middleware('throttle:30,1')->group(function () {
+    Route::get('/', [Handler::class, 'viewUserLogin'])->name('user.view.login');
+    Route::post('/login', [Handler::class, 'userLogin'])->name('user.do.login');
+    Route::post('/forgot-password', [Handler::class, 'userForgotPassword'])->name('user.do.forgot.password');
+    Route::prefix('md-admin')->group(function () {
+        Route::get('/', [Handler::class, 'viewAdminLogin'])->name('admin.view.login');
+        Route::post('/login', [Handler::class, 'adminLogin'])->name('admin.do.login');
+    });
 });
