18c12ec533668e75de107219c96b0b0e9364b6af
xamppsul <samsul.marif100@gmail.com>
2025-05-06 11:51:08 +0800

    middleware:guard setup route staff dosen

--- app/Modules/StaffOrDosen/routes/web.php
+++ app/Modules/StaffOrDosen/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/staff-dosen', function () {
-    return 'hello dari staff dosen';
+Route::middleware(['throttle:30,1', 'auth:user'])->group(function () {
+    Route::get('/staff-dosen', function () {
+        return 'hello dari staff dosen';
+    });
 });
