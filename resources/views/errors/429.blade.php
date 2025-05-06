295f68b7c01776db55e6dec31e92060ab1102a0c
xamppsul <samsul.marif100@gmail.com>
2025-05-06 11:44:13 +0800

    429 template

--- resources/views/errors/429.blade.php
+++ resources/views/errors/429.blade.php
@@ -1,5 +1,29 @@
-@extends('errors::minimal')
+@extends('errors.layouts.master')
+@section('title', __('Many Request | MD Febi UMK'))
 
-@section('title', __('Too Many Requests'))
-@section('code', '429')
-@section('message', __('Too Many Requests'))
+@section('content')
+    <div class='universal-nav'>
+        <img alt="freeCodeCamp.org" src="{{ asset('assets/images/logo/umk.png') }}">
+    </div>
+    <div class="error-middle">
+        <h2>
+            429 | Too many requests.
+        </h2>
+
+        <br />
+
+        <h3>
+            We are getting too many requests from your IP address and temporarily blocking access to this part of
+            MD Project
+        </h3>
+
+        <h3>
+            This should be resolved shortly.
+        </h3>
+        <h3>
+            Please try again in while. If you keep seeing this error, please <a href="https://google.com" target="_blank"
+                rel="noopener">find google</a>.
+        </h3>
+
+    </div>
+@endsection
---
+++ resources/views/errors/layouts/css.blade.php
@@ -0,0 +1,86 @@
+<style>
+    @import url("https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap");
+
+    html {
+        background-color: #ffffff;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    body {
+        font-family: "Roboto Mono", monospace;
+        color: #0a0a23;
+        font-size: 18px;
+        padding: 1em;
+        line-height: 1.4;
+        text-align: left;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-top: 10vw;
+    }
+
+    .error-middle {
+        display: block;
+        width: 80vw;
+    }
+
+    a {
+        color: #0a0a23;
+        text-decoration: underline;
+    }
+
+    a:hover {
+        color: #f5f6f7;
+        background-color: #1b1b32;
+        cursor: pointer;
+        text-decoration: none;
+    }
+
+    a:active {
+        box-shadow: none;
+        top: 5px;
+    }
+
+    .cloudflare-block {
+        padding: 1em;
+        margin: 1em;
+    }
+
+    .cloudflare-block h1 {
+        font-size: 1em;
+    }
+
+    .cloudflare-block p,
+    .cloudflare-block ul,
+    .cloudflare-block li {
+        font-size: 1em;
+    }
+
+    .universal-nav {
+        margin: 0px;
+        width: 100vw;
+        height: 38px;
+        background-color: #0a0a23;
+        position: fixed;
+        top: 0px;
+        display: flex;
+        justify-content: center;
+    }
+
+    .universal-nav img {
+        margin: 7px 0px;
+        height: 24px;
+    }
+
+    .youtube-embed {
+        text-align: center;
+    }
+
+    @media (min-width: 768px) {
+        .error-middle {
+            width: 60vw;
+        }
+    }
+</style>
