7609f27f8ceceaadd57d007722e29fdea6198791
xamppsul <samsul.marif100@gmail.com>
2025-05-15 06:01:38 +0800

    setup request user master on admin

---
+++ app/Src/Request/Admin/Master/UserMasterRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Src\Request\Admin\Master;
+
+class UserMasterRequest
+{
+
+    public static function postRequestData($request): array
+    {
+        return $request->validate([
+            'name' => 'required|string',
+            'username' => 'required|unique:users,username',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+            'roles_id' => 'required|exists:roles,id',
+        ], [
+            'unique' => ':attribute sudah tersedia harap gunakan yang lain',
+            'required' => ':attribute wajib di isi',
+            'exists' => ':attribute tidak ditemukan di database',
+            'email' => ':attribute format salah'
+        ]);
+    }
+
+    public static function updateRequestData($request): array
+    {
+        return $request->validate([
+            'name' => 'required|string',
+            'username' => 'required|unique:users,username',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+            'roles_id' => 'required|exists:roles,id',
+        ], [
+            'unique' => ':attribute sudah tersedia harap gunakan yang lain',
+            'required' => ':attribute wajib di isi',
+            'exists' => ':attribute tidak ditemukan di database',
+            'email' => ':attribute format salah'
+        ]);
+    }
+}
