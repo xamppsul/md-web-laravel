7ee0e49781433f4bc063f305608502607e123afc
xamppsul <samsul.marif100@gmail.com>
2025-05-15 15:58:52 +0800

    back app <- App route base name directory

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 /**
  * instance routes every module
  */
-require base_path('App/Modules/Authentication/routes/web.php');
-require base_path('App/Modules/Administrator/routes/web.php');
-require base_path('App/Modules/StaffOrDosen/routes/web.php');
-require base_path('App/Modules/UppsOrFakultas/routes/web.php');
+require base_path('app/Modules/Authentication/routes/web.php');
+require base_path('app/Modules/Administrator/routes/web.php');
+require base_path('app/Modules/StaffOrDosen/routes/web.php');
+require base_path('app/Modules/UppsOrFakultas/routes/web.php');
