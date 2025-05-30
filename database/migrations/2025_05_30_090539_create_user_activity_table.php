e8311596dc5a4d70887f5abf657c963d2c59d182
xamppsul <samsul.marif100@gmail.com>
2025-05-30 09:09:10 +0800

    make struct table activity users

---
+++ database/migrations/2025_05_30_090539_create_user_activity_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_activity', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('users_id')->references('id')->on('users');
+            $table->string('activity_type');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_activity');
+    }
+};
